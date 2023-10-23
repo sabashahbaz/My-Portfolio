ba1f78f55604e882560e3308e4ca1b34a621fbbf
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-10-23 16:28:42 -0400

    carousel

--- my-portfolio/src/components/Banner.js
+++ my-portfolio/src/components/Banner.js
@@ -6,10 +6,10 @@ import headerImg from "../assets/img/header-img.svg"
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Software Engineer","Web Developer", "Web Designer", "UI/UX Designer"]
+    const toRotate = ["Software Engineer","Product Designer", "Full-Stack Web Developer", "UI/UX Designer"]
     const [text, setText] = useState('')
     const [delta, setDelta] = useState(300 - Math.random() * 100)
-    const period = 2000;
+    const period = 500;
 
     useEffect(()=> {
         let ticker = setInterval(()=> {
@@ -42,20 +42,19 @@ export const Banner = () => {
 
     return (
         <section className="banner" id="home">
-            <Container>
-                <Row className="align-items-center">
-                    <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm webdecoded`}<span className="wrap">{text}</span></h1>
-                        <p>random text</p>
-                        <button onClick={()=> console.log('connect')}>Let's Connect<ArrowRightCircle size={25}/></button>
-                    </Col>
-                    <Col xs={12} md={6} xl={7}>
-                        <img src={headerImg} alt="Header Img"/>
-                    </Col>
-                </Row>
-            </Container>
-
-        </section>
+    <Container>
+        <Row className="align-items-center">
+            <Col xs={12} md={6} xl={6}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`I'm a `}<span className="wrap">{text}</span></h1>
+                <p>random text</p>
+                <button onClick={() => console.log('connect')}>Let's Connect<ArrowRightCircle size={25}/></button>
+            </Col>
+            <Col xs={12} md={6} xl={6}>
+                <img src={headerImg} alt="Header Img"/>
+            </Col>
+        </Row>
+    </Container>
+</section>
     )
 }
