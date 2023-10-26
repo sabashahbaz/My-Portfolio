2bd1f1f0eccdd2d13e839635c25c08e570d6c20f
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-10-26 15:09:25 -0400

    resume

--- my-portfolio/src/components/Resume.js
+++ my-portfolio/src/components/Resume.js
@@ -1,16 +1,29 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import resume from '../assets/resume.pdf'
-import PdfViewer from './PdfViewer'
-
-
+import my_resume from '../assets/my-resume.png'
+import download from '../assets/download.png'
+import CSS from '../Resume.css'
 
 export const Resume = () => {
+    return(
+        <div className="text-bg">
+            
+            <div className="resume-container">
+            <div className = "head-button">
+            <h2 className="resume-header">My Resume </h2>
+            <a href={resume} download="Saba-Shahbaz_Resume">
+            <img className='download' src={download} />
+            <button className='download-button'>Download Resume</button>
+            </a>
 
-    return (
-        <div>
-            <div>
-            <PdfViewer pdf={resume}/>
             </div>
+
+            <div className="resume-img-container">
+                <img className="resume-img" src={my_resume} alt="Resume"/>
+            </div>
+
+            </div>
+
         </div>
-    );
-};
+    )
+}
